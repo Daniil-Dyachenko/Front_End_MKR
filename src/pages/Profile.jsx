a099c3243f4e67da1100a767545fc28a43a645aa
Daniil-Dyachenko <159638173+Daniil-Dyachenko@users.noreply.github.com>
2024-12-29 04:06:12 +0200

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,12 +18,14 @@ function Profile() {
 
     const fetchData = async () => {
       try {
-        const userInfoData = await getUserInfo(username);
-        const userPostsData = await getUserPosts(username, token);
+        const [userInfoData, userPostsData] = await Promise.all([
+          getUserInfo(username),
+          getUserPosts(username, token),
+        ]);
         setUserInfo(userInfoData);
         setUserPosts(userPostsData);
-      } catch (error) {
-        setError('Неможливо завантажeння даних');
+      } catch (err) {
+        setError('Неможливо завантажити дані.');
       } finally {
         setIsLoading(false);
       }
@@ -42,7 +44,7 @@ function Profile() {
       <div className="profile">
         <div className="profile-header">
           <h1 className="profile-username">{userInfo?.full_name || username}</h1>
-          <p className="profile-bio">{userInfo?.bio || 'Біографія відсутня.'}</p>
+          <p className="profile-bio">{userInfo?.bio || 'Опис профілю відсутній.'}</p>
         </div>
         {error ? (
           <div className="error">
@@ -55,20 +57,19 @@ function Profile() {
               <ul className="posts-list">
                 {userPosts.map((post) => (
                   <li
-                    onClick={() => navigate(`/post/${post.author.username}/${post.id}`)}
                     key={post.id}
-                    className="post-item">
+                    className="post-item"
+                    onClick={() => navigate(`/post/${post.author.username}/${post.id}`)}
+                  >
                     <div className="post-header">
                       <h3 className="post-author">{post.author.full_name}</h3>
                       <span className="post-author-username">@{post.author.username}</span>
                     </div>
                     <p className="post-content">{post.content}</p>
                     <div className="post-info">
-                      <span className="post-likes">
-                        ❤️ {post.likes}
-                      </span>
+                      <span className="post-likes">❤️ {post.likes}</span>
                       <span className="post-liked">
-                        {post.is_liked ? 'Вам сподобалось' : "Вам не сподобалось"}
+                        {post.is_liked ? 'Вам сподобалось' : 'Вам не сподобалось'}
                       </span>
                       <span className="post-created-at">
                         {new Date(post.created_at).toLocaleString()}
